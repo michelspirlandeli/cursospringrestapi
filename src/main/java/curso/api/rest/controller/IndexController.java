ab94b45e5ffd74ecd6b784b2d079b9b3f8fe523b
michelspirlandeli <michelspirlandeli10@gmail.com>
2019-08-19 22:13:36 -0300

    Add @RequestParam

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json" )
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Usuario Rest Spring boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam(value = "nome", required = true) String nome) {
+		System.out.println("Parametro sendo recebido " + nome);
+		return new ResponseEntity("Olá Usuario Rest Spring boot seu nome é " + nome, HttpStatus.OK);
 	}
 }
